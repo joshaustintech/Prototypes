b16e72b3dbb1aa604745cffbe8534818b1208bad
Josh Austin <83935880+joshaustintech@users.noreply.github.com>
2024-03-21 23:17:12 -0500

    Started "Legacy" concept design

---
+++ Legacy/Legacy/LegacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegacyApp.swift
+//  Legacy
+//
+//  Created by Joshua Austin on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LegacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
